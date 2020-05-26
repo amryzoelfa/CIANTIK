f80fb3f0d3ceabc1b2cbf38d62af10f4c4801bb8
Amry Zulfa Al Husna <amryzoelfa@gmail.com>
2020-05-26 17:49:46 +0700

    Update Dokter 26/5
    Controller Dokter dan Model Dokter ~Amry

--- application/controllers/Dokter.php
+++ application/controllers/Dokter.php
@@ -11,12 +11,14 @@ class Dokter extends CI_Controller
 
     function index()
     {
-    
         $data['jumlahPasien'] = $this->Dashboard_model->getJumlahPasien();
         $data['jumlahDokter'] = $this->Dashboard_model->getJumlahDokter();
         $data['jumlahApoteker'] = $this->Dashboard_model->getJumlahApoteker();
         $data['jumlahKunjunganHari'] = $this->Dashboard_model->getKunjunganHari();
-        $this->template->tampil('Dokter/dashboard_view', $data);
+        $data['jumlahAntrian'] = $this->Dashboard_model->getTotalAntrian();
+        $data['jumlahAUmum'] = $this->Dashboard_model->getAntrianUmum();
+        $data['jumlahAGigi'] = $this->Dashboard_model->getAntrianGigi();
+        $this->template->tampil('Dokter/dDashboard_view', $data);
     }
 
     // function antrianUmum()

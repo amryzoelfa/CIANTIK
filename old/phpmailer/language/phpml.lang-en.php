42a24f48c5ff70915f79be9249390c8b80bd6768
ratna712 <ratnadsafitri@gmail.com>
2020-05-28 18:16:51 +0700

    css, vendors, js, library, img, fonts (old)

---
+++ old/phpmailer/language/phpml.lang-en.php
@@ -0,0 +1,131 @@
+<?php
+/*~ phpml.lang-en.php
+.---------------------------------------------------------------------------.
+|  Software: PHPMailer-ML - English Language File                           |
+'---------------------------------------------------------------------------'
+
+ * @author Andy Prevost
+ */
+
+$PHPML_LANG = array();
+
+/* labels */
+$PHPML_LANG["add_new_list"]             = 'Add List';
+$PHPML_LANG["added"]                    = 'Added';
+$PHPML_LANG["adminBcc"]                 = 'Admin Bcc';
+$PHPML_LANG["adminBcc_explain"]         = '<small>If checked - will send admin Bcc on Subscription Confirmation</small>';
+$PHPML_LANG["appname"]                  = 'Application Name';
+$PHPML_LANG["bcc"]                      = 'Bcc';
+$PHPML_LANG["campaign"]                 = 'Campaign';
+$PHPML_LANG["campaigns"]                = 'Campaigns';
+$PHPML_LANG["click_continue"]           = 'Click Here To Continue ...';
+$PHPML_LANG["configuration"]            = 'Configuration';
+$PHPML_LANG["continue"]                 = 'Continue';
+$PHPML_LANG["comma"]                    = 'Comma';
+$PHPML_LANG["CSVheader"]                = 'CSV file contains header row';
+$PHPML_LANG["db_host"]                  = 'Database Host';
+$PHPML_LANG["db_name"]                  = 'Database Name';
+$PHPML_LANG["db_user"]                  = 'Database Username';
+$PHPML_LANG["db_pass"]                  = 'Database Password';
+$PHPML_LANG["databases"]                = 'Databases';
+$PHPML_LANG["deleted"]                  = 'Deleted';
+$PHPML_LANG["deleteselected"]           = 'Delete Selected';
+$PHPML_LANG["edit"]                     = 'Edit';
+$PHPML_LANG["english"]                  = 'English';
+$PHPML_LANG["email_format"]             = 'Format';
+$PHPML_LANG["email_htmltext"]           = 'HTML &amp; Text';
+$PHPML_LANG["email_textonly"]           = 'Text only';
+$PHPML_LANG["error_uploading_file"]     = 'Error uploading file';
+$PHPML_LANG["false"]                    = 'False';
+$PHPML_LANG["field_enclosure"]          = 'Field Enclosure';
+$PHPML_LANG["field_separator"]          = 'Field Separator';
+$PHPML_LANG["file_not_writable"]        = 'The file is not writable, nothing to do!';
+$PHPML_LANG["format"]                   = 'Format';
+$PHPML_LANG["from"]                     = 'From';
+$PHPML_LANG["fromname"]                 = 'From (name)';
+$PHPML_LANG["fromaddy"]                 = 'From (email)';
+$PHPML_LANG["go"]                       = 'Go';
+$PHPML_LANG["import"]                   = 'Import CSV';
+$PHPML_LANG["import_options"]           = 'Import File Options';
+$PHPML_LANG["list_records"]             = 'List records';
+$PHPML_LANG["mailinglist"]              = 'Mailing List';
+$PHPML_LANG["mailinglists"]             = 'Mailing Lists';
+$PHPML_LANG["makedefault"]              = 'Make Default';
+$PHPML_LANG["language"]                 = 'Language';
+$PHPML_LANG["list"]                     = 'List';
+$PHPML_LANG["lists"]                    = 'Lists';
+$PHPML_LANG["message"]                  = 'Message';
+$PHPML_LANG["name_email"]               = 'Email';
+$PHPML_LANG["name_first"]               = 'First Name';
+$PHPML_LANG["name_last"]                = 'Last Name';
+$PHPML_LANG["need_to_map"]              = 'Need to map row1 to database fields';
+$PHPML_LANG["none"]                     = 'None';
+$PHPML_LANG["or"]                       = 'or';
+$PHPML_LANG["preferences"]              = 'Preferences';
+$PHPML_LANG["preview"]                  = 'Preview';
+$PHPML_LANG["quotedouble"]              = 'Double Quote';
+$PHPML_LANG["quotesingle"]              = 'Single Quote (Apostrophe)';
+$PHPML_LANG["select"]                   = 'Select';
+$PHPML_LANG["send"]                     = 'Send';
+$PHPML_LANG["sendemails"]               = 'Send Emails';
+$PHPML_LANG["sendemails_explain"]       = '<small>If checked - will send emails / if NOT checked - will disable sending emails</small>';
+$PHPML_LANG["sent"]                     = 'Sent';
+$PHPML_LANG["settings"]                 = 'Configuration';
+$PHPML_LANG["settings_email"]           = 'Outbound Email Settings';
+$PHPML_LANG["settings_db"]              = 'Database Settings';
+$PHPML_LANG["SMTPhost"]                 = 'SMTP Host';
+$PHPML_LANG["SMTPauth"]                 = 'SMTP Authentication';
+$PHPML_LANG["SMTPusername"]             = 'SMTP Username';
+$PHPML_LANG["SMTPpassword"]             = 'SMTP Password';
+$PHPML_LANG["SMTPport"]                 = 'SMTP Port';
+$PHPML_LANG["save"]                     = 'Save';
+$PHPML_LANG["subject"]                  = 'Subject';
+$PHPML_LANG["submit"]                   = 'Submit';
+$PHPML_LANG["subscriber_records"]       = 'Subscriber records';
+$PHPML_LANG["subscriber"]               = 'Subscriber';
+$PHPML_LANG["subscribers"]              = 'Subscribers';
+$PHPML_LANG["tab"]                      = 'Tab';
+$PHPML_LANG["to"]                       = 'To';
+$PHPML_LANG["thankyou"]                 = 'Thank You!';
+$PHPML_LANG["true"]                     = 'True';
+$PHPML_LANG["type"]                     = 'Type';
+$PHPML_LANG["updated"]                  = 'Updated';
+$PHPML_LANG["updating"]                 = 'Updating';
+$PHPML_LANG["uploadfile"]               = 'Upload File';
+$PHPML_LANG["usebcc"]                   = 'Use BCC';
+$PHPML_LANG["usebcc_explain"]           = '<small>If checked - will use Bcc to send / if NOT checked - will send individual emails (note: SMTP will NOT use Bcc).</small>';
+$PHPML_LANG["which_list"]               = 'Use List';
+
+/* messages / phrases */
+$PHPML_LANG["lib_not_found"]            = 'ERROR: PHPMailer Library was not found ';
+                                          'in your path <i>or</i> at';
+$PHPML_LANG["csv_not_found"]            = 'ERROR: Failed to open CSV list file.';
+$PHPML_LANG["no_email"]                 = 'ERROR: No email address specified!';
+$PHPML_LANG["no_display"]               = 'ERROR: Nothing to display!';
+$PHPML_LANG["error_sending"]            = 'ERROR: While sending email';
+$PHPML_LANG["error_query"]              = 'ERROR: Unable to execute query ';
+$PHPML_LANG["error_unsubscribe"]        = 'ERROR: The unsubscribe link you provided is not valid.';
+$PHPML_LANG["success_sending"]          = 'Message has been sent successfully.';
+$PHPML_LANG["send_another"]             = 'Send another message?';
+$PHPML_LANG["compose_message"]          = 'Compose a message';
+$PHPML_LANG["compose1"]                 = 'Compose a message for the ';
+$PHPML_LANG["compose2"]                 = ' Mailing List';
+$PHPML_LANG["Confirmed"]                = 'Email Address is now confirmed, Thank You!';
+$PHPML_LANG["not_subscribed"]           = ' is not subscribed.';
+$PHPML_LANG["now_registered_to"]        = ' is now registered to the ';
+$PHPML_LANG["now_registered"]           = ' is now registered ';
+$PHPML_LANG["now_subscribed_to"]        = ' is now subscribed to the ';
+$PHPML_LANG["now_subscribed"]           = ' is now subscribed ';
+$PHPML_LANG["now_unsubscribed_from"]    = ' is now unsubscribed from the ';
+$PHPML_LANG["selfsubscribe1"]           = 'This email address was used to subscribe to the ' .
+                                          $phpml['ListName'] . ' mailing lists. If you made this ' .
+                                          'request, please click here:' . "\n";
+$PHPML_LANG["selfsubscribe2"]           = "\n\n" . 'If you did not request this action, please ' .
+                                          'ignore this email - you will receive no further communications.';
+$PHPML_LANG["subscribe"]                = 'To subscribe, click here';
+$PHPML_LANG["subscribe_confirm"]        = ' ... you will receive an email to confirm your subscription ';
+$PHPML_LANG["already_subscribed"]       = ' is already subscribed.';
+$PHPML_LANG["unsubscribe"]              = 'To unsubscribe, click here';
+$PHPML_LANG["unsubscribed"]             = ' unsubscribed';
+
+?>

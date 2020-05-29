a6a53f886017e6611c7215445266ebcc1fb5f9a8
Amry Zulfa Al Husna <amryzoelfa@gmail.com>
2020-05-29 20:22:33 +0700

    Update Admin.php
    Update controller Admin.php function Laporan &  cetak 29/05 ~Amry

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -6,6 +6,7 @@ class Admin extends CI_Controller
         parent::__construct();
         $this->load->model('Admin_model');
         $this->load->model('Dashboard_model');
+        $this->load->model('Laporan_model');
         $this->load->library('template');
     }
 
@@ -17,6 +18,8 @@ class Admin extends CI_Controller
         $data['jumlahDokter'] = $this->Dashboard_model->getJumlahDokter();
         $data['jumlahApoteker'] = $this->Dashboard_model->getJumlahApoteker();
         $data['jumlahKunjunganHari'] = $this->Dashboard_model->getKunjunganHari();
+        $data['jumlahAUmum'] = $this->Dashboard_model->getAntrianUmum();
+        $data['jumlahAGigi'] = $this->Dashboard_model->getAntrianGigi();
         $this->template->tampil('Admin/dashboard_view', $data);
     }
 
@@ -68,12 +71,95 @@ class Admin extends CI_Controller
             redirect('Master/DataApoteker');
         }
     }
-    public function antrian_umum(){
+    public function antrian_umum()
+    {
         $data['antrian'] = $this->Admin_model->getAntrianUmum();
         $this->template->tampil('Admin/antrian_umum', $data);
     }
-    public function antrian_gigi(){
+    public function antrian_gigi()
+    {
         $data['antrian'] = $this->Admin_model->getAntrianGigi();
         $this->template->tampil('Admin/antrian_gigi');
+    }
+
+    public function Laporan()
+    {
+        if (isset($_GET['filter']) && !empty($_GET['filter'])) { // Cek apakah user telah memilih filter dan klik tombol tampilkan
+            $filter = $_GET['filter']; // Ambil data filder yang dipilih user
+
+            if ($filter == '1') { // Jika filter nya 1 (per tanggal)
+                $tgl =  $_GET['tanggal'];
+
+                $ket = 'Data Transaksi Tanggal ' . date('d-m-y', strtotime($tgl));
+                $url_cetak = 'Admin/cetak?filter=1&tanggal=' . $tgl;
+                $transaksi = $this->Laporan_model->view_by_date($tgl); // Panggil fungsi view_by_date yang ada di Laporan_model
+            } else if ($filter == '2') { // Jika filter nya 2 (per bulan)
+                $bulan = $_GET['bulan'];
+                $tahun = $_GET['tahun'];
+                $nama_bulan = array('', 'Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember');
+
+                $ket = 'Data Transaksi Bulan ' . $nama_bulan[$bulan] . ' ' . $tahun;
+                $url_cetak = 'Admin/cetak?filter=2&bulan=' . $bulan . '&tahun=' . $tahun;
+                $transaksi = $this->Laporan_model->view_by_month($bulan, $tahun); // Panggil fungsi view_by_month yang ada di Laporan_model
+            } else { // Jika filter nya 3 (per tahun)
+                $tahun = $_GET['tahun'];
+
+                $ket = 'Data Transaksi Tahun ' . $tahun;
+                $url_cetak = 'Admin/cetak?filter=3&tahun=' . $tahun;
+                $transaksi = $this->Laporan_model->view_by_year($tahun); // Panggil fungsi view_by_year yang ada di Laporan_model
+            }
+        } else { // Jika user tidak mengklik tombol tampilkan
+            $ket = 'Semua Data Transaksi';
+            $url_cetak = 'Admin/cetak';
+            $transaksi = $this->Laporan_model->view_all(); // Panggil fungsi view_all yang ada di Laporan_model
+        }
+
+        $data['ket'] = $ket;
+        $data['url_cetak'] = base_url($url_cetak);
+        $data['transaksi'] = $transaksi;
+        $data['option_tahun'] = $this->Laporan_model->option_tahun();
+        $this->load->view('Admin/laporan_view', $data);
+    }
+
+    public function cetak()
+    {
+        if (isset($_GET['filter']) && !empty($_GET['filter'])) { // Cek apakah user telah memilih filter dan klik tombol tampilkan
+            $filter = $_GET['filter']; // Ambil data filder yang dipilih user
+
+            if ($filter == '1') { // Jika filter nya 1 (per tanggal)
+                $tgl = $_GET['tanggal'];
+
+                $ket = 'Data Transaksi Tanggal ' . date('d-m-y', strtotime($tgl));
+                $transaksi = $this->Laporan_model->view_by_date($tgl); // Panggil fungsi view_by_date yang ada di Laporan_model
+            } else if ($filter == '2') { // Jika filter nya 2 (per bulan)
+                $bulan = $_GET['bulan'];
+                $tahun = $_GET['tahun'];
+                $nama_bulan = array('', 'Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember');
+
+                $ket = 'Data Transaksi Bulan ' . $nama_bulan[$bulan] . ' ' . $tahun;
+                $transaksi = $this->Laporan_model->view_by_month($bulan, $tahun); // Panggil fungsi view_by_month yang ada di Laporan_model
+            } else { // Jika filter nya 3 (per tahun)
+                $tahun = $_GET['tahun'];
+
+                $ket = 'Data Transaksi Tahun ' . $tahun;
+                $transaksi = $this->Laporan_model->view_by_year($tahun); // Panggil fungsi view_by_year yang ada di Laporan_model
+            }
+        } else { // Jika user tidak mengklik tombol tampilkan
+            $ket = 'Semua Data Transaksi';
+            $transaksi = $this->Laporan_model->view_all(); // Panggil fungsi view_all yang ada di Laporan_model
+        }
+
+        $data['ket'] = $ket;
+        $data['transaksi'] = $transaksi;
+
+        ob_start();
+        $this->load->view('print', $data);
+        $html = ob_get_contents();
+        ob_end_clean();
+
+        require_once('./assets/plugin/html2pdf/html2pdf.class.php');
+        $pdf = new HTML2PDF('P', 'A4', 'en');
+        $pdf->WriteHTML($html);
+        $pdf->Output('Data Transaksi.pdf', 'D');
     }
 }

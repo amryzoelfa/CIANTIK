1ad9da2fa20e7ab409647c2ddaee1d71bb039a9d
Amry Zulfa Al Husna <amryzoelfa@gmail.com>
2020-05-29 20:21:25 +0700

    Update js.php
    New _Template js.php 29/05 ~Amry

--- application/views/_Template/js.php
+++ application/views/_Template/js.php
@@ -20,3 +20,30 @@
       $('#dataTable').DataTable();
     });
   </script>
+
+  <script src="<?php echo base_url('assets/plugin/jquery.min.js') ?>"></script> <!-- Load file jquery -->
+  <script src="<?php echo base_url('assets/plugin/jquery-ui/jquery-ui.min.js') ?>"></script>
+  <script>
+    $(document).ready(function() { // Ketika halaman selesai di load
+      $('.input-tanggal').datepicker({
+        dateFormat: 'yy-mm-dd' // Set format tanggalnya jadi yyyy-mm-dd
+      });
+
+      $('#form-tanggal, #form-bulan, #form-tahun').hide(); // Sebagai default kita sembunyikan form filter tanggal, bulan & tahunnya
+
+      $('#filter').change(function() { // Ketika user memilih filter
+        if ($(this).val() == '1') { // Jika filter nya 1 (per tanggal)
+          $('#form-bulan, #form-tahun').hide(); // Sembunyikan form bulan dan tahun
+          $('#form-tanggal').show(); // Tampilkan form tanggal
+        } else if ($(this).val() == '2') { // Jika filter nya 2 (per bulan)
+          $('#form-tanggal').hide(); // Sembunyikan form tanggal
+          $('#form-bulan, #form-tahun').show(); // Tampilkan form bulan dan tahun
+        } else { // Jika filternya 3 (per tahun)
+          $('#form-tanggal, #form-bulan').hide(); // Sembunyikan form tanggal dan bulan
+          $('#form-tahun').show(); // Tampilkan form tahun
+        }
+
+        $('#form-tanggal input, #form-bulan select, #form-tahun select').val(''); // Clear data pada textbox tanggal, combobox bulan & tahun
+      })
+    })
+  </script>

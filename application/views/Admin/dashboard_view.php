50dfd2f3b413cb12288c8b7f8b22d9718c2efdf4
Amry Zulfa Al Husna <amryzoelfa@gmail.com>
2020-05-29 20:20:03 +0700

    Update dashboard_view.php
    New view dashboard_view.php Admin 29/05 ~Amry

--- application/views/Admin/dashboard_view.php
+++ application/views/Admin/dashboard_view.php
@@ -97,6 +97,40 @@
         </div>
     </div>
 
+    <!-- Earnings (Monthly) Card Example -->
+    <div class="col-xl-3 col-md-6 mb-4">
+        <div class="card border-left-success shadow h-100 py-2">
+            <div class="card-body">
+                <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                        <div class="text-sm font-weight-bold text-success text-uppercase mb-1">Jumlah Antrian Umum</div>
+                        <div class="h5 mb-0 font-weight-bold text-gray-800"><?= $jumlahAUmum['total_umum']; ?></div>
+                    </div>
+                    <div class="col-auto">
+                        <i class="fas fa-user-nurse fa-2x text-gray-300"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Earnings (Monthly) Card Example -->
+    <div class="col-xl-3 col-md-6 mb-4">
+        <div class="card border-left-primary shadow h-100 py-2">
+            <div class="card-body">
+                <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                        <div class="text-sm font-weight-bold text-primary text-uppercase mb-1">Jumlah Antrian Gigi</div>
+                        <div class="h5 mb-0 font-weight-bold text-gray-800"><?= $jumlahAGigi['total_gigi']; ?></div>
+                    </div>
+                    <div class="col-auto">
+                        <i class="fas fa-users fa-2x text-gray-300"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
 </div>
 
 <!-- <br><br><br><br><br><br><br><br><br><br> -->

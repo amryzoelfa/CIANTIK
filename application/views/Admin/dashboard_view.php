30972f52b34b73b5517bf9f2c170d772c2593dc6
Amry Zulfa Al Husna <amryzoelfa@gmail.com>
2020-05-30 20:35:43 +0700

    Update dashboard_view.php
    Update View dashboard_view Admin 30/5 ~Amry

--- application/views/Admin/dashboard_view.php
+++ application/views/Admin/dashboard_view.php
@@ -99,11 +99,11 @@
 
     <!-- Earnings (Monthly) Card Example -->
     <div class="col-xl-3 col-md-6 mb-4">
-        <div class="card border-left-success shadow h-100 py-2">
+        <div class="card border-left-primary shadow h-100 py-2">
             <div class="card-body">
                 <div class="row no-gutters align-items-center">
                     <div class="col mr-2">
-                        <div class="text-sm font-weight-bold text-success text-uppercase mb-1">Jumlah Antrian Umum</div>
+                        <div class="text-sm font-weight-bold text-primary text-uppercase mb-1">Jumlah Antrian Umum</div>
                         <div class="h5 mb-0 font-weight-bold text-gray-800"><?= $jumlahAUmum['total_umum']; ?></div>
                     </div>
                     <div class="col-auto">
@@ -116,11 +116,11 @@
 
     <!-- Earnings (Monthly) Card Example -->
     <div class="col-xl-3 col-md-6 mb-4">
-        <div class="card border-left-primary shadow h-100 py-2">
+        <div class="card border-left-success shadow h-100 py-2">
             <div class="card-body">
                 <div class="row no-gutters align-items-center">
                     <div class="col mr-2">
-                        <div class="text-sm font-weight-bold text-primary text-uppercase mb-1">Jumlah Antrian Gigi</div>
+                        <div class="text-sm font-weight-bold text-success text-uppercase mb-1">Jumlah Antrian Gigi</div>
                         <div class="h5 mb-0 font-weight-bold text-gray-800"><?= $jumlahAGigi['total_gigi']; ?></div>
                     </div>
                     <div class="col-auto">

6a1fdd1ff13ad6b5e0205b7caf7b2856ddf290a5
ratna712 <ratnadsafitri@gmail.com>
2020-06-05 21:19:43 +0700

    update dokter_model
    rubah nama field

--- application/models/Dokter_model.php
+++ application/models/Dokter_model.php
@@ -5,13 +5,13 @@ class Dokter_model extends CI_Model
 {
 	function drUmum()
 	{
-		$query1 = "SELECT tb_antrian.no_antrian, tb_user.nama, tb_periksa.id_status_periksa, tb_periksa.id_user FROM tb_antrian , tb_user , tb_periksa WHERE tb_antrian.id_user =  tb_user.id_user AND tb_user.id_user = tb_periksa.id_user AND tb_periksa.tanggal_periksa = CURRENT_DATE() AND tb_periksa.id_poli = 1 AND tb_antrian.id_poli=1 AND tb_antrian.tanggal = CURRENT_DATE()  GROUP BY tb_periksa.id_periksa";
-		return $this->db->query($query1)->row_array();
+		$query1 = "SELECT tb_periksa.no_antrian, tb_user.nama, tb_periksa.tanggal, tb_periksa.id_status_periksa, tb_periksa.id_user FROM tb_user , tb_periksa WHERE tb_user.id_user = tb_periksa.id_user AND tb_periksa.tanggal = CURRENT_DATE() AND tb_periksa.id_poli = 1";
+		return $this->db->query($query1)->result();
 	}
 	function drGigi()
 	{
-		$query2 = "SELECT tb_antrian.no_antrian, tb_antrian.tanggal, tb_user.no_identitas, tb_user.nama, tb_periksa.id_status_periksa, tb_periksa.id_status_obat, tb_periksa.id_user FROM tb_antrian , tb_user , tb_periksa WHERE tb_antrian.id_user = tb_user.id_user AND tb_user.id_user = tb_periksa.id_user AND tb_periksa.tanggal_periksa = CURRENT_DATE() AND tb_periksa.id_poli = 2 AND tb_antrian.id_poli=2 AND tb_antrian.tanggal = CURRENT_DATE() GROUP BY tb_antrian.id_antrian";
-		return $this->db->query($query2)->row_array();
+		$query2 = "SELECT tb_periksa.no_antrian, tb_periksa.tanggal, tb_user.nama, tb_periksa.id_status_periksa,tb_periksa.id_user FROM tb_user , tb_periksa WHERE tb_user.id_user = tb_periksa.id_user AND tb_periksa.tanggal = CURRENT_DATE() AND tb_periksa.id_poli = 2";
+		return $this->db->query($query2)->result();
 	}
 	function edit_data_umum($where){
 		$query = "SELECT * FROM tb_periksa JOIN tb_user ON tb_user.id_user = tb_periksa.id_user WHERE tb_periksa.tanggal = CURRENT_DATE() and tb_periksa.id_poli=1 and tb_periksa.id_status_periksa = 1 and tb_periksa.id_user = '$where' GROUP BY tb_periksa.id_user";
@@ -97,3 +97,4 @@ class Dokter_model extends CI_Model
 		$this->db->update('tb_periksa',$ubah);
 	}
 }
+?>
